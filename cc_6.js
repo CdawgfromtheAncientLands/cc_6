ba2d598c2b855e4a2929a4add5560187fc9d4ad1
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-06-04 16:59:28 -0400

    Set up HTML canvas for animation.

---
+++ cc_6.js
@@ -0,0 +1,3 @@
+//U94140233
+//Implement a JavaScript constructor class named Ball.
+//Initialize properties such as x, y, radius, dx (change in x-coordinate), dy (change in y-coordinate), and color.
