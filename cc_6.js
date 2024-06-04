94fa026d43fc3f2f5fea894dc34c44924d2413fc
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-06-04 17:02:00 -0400

    Create Ball constructor with initial properties

--- cc_6.js
+++ cc_6.js
@@ -1,3 +1,14 @@
 //U94140233
 //Implement a JavaScript constructor class named Ball.
 //Initialize properties such as x, y, radius, dx (change in x-coordinate), dy (change in y-coordinate), and color.
+class Ball {
+
+    constructor(ballStartX, ballStartY, ballRadius, ballDX, ballDY, ballColor) {
+        this.ballStartX = ballStartX;
+        this.ballStartY = ballStartY;
+        this.ballRadius = ballRadius;
+        this.ballDX = ballDX;
+        this.ballDY = ballDY;
+        this.ballColor = ballColor;
+    }
+}
