b42ef3e392269e0abd6820937cbcdb70cd0dcaa6
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-06-04 17:35:42 -0400

    Implement draw method for Ball class.

--- cc_6.js
+++ cc_6.js
@@ -1,6 +1,10 @@
 //U94140233
 //Implement a JavaScript constructor class named Ball.
 //Initialize properties such as x, y, radius, dx (change in x-coordinate), dy (change in y-coordinate), and color.
+
+const canvas = document.getElementById('ballCanvas');
+const ctx = canvas.getContext('2d');
+
 class Ball {
 
     constructor(ballStartX, ballStartY, ballRadius, ballDX, ballDY, ballColor) {
@@ -11,4 +15,15 @@ class Ball {
         this.ballDY = ballDY;
         this.ballColor = ballColor;
     }
+//Implement a method within the Ball constructor to draw the ball on the canvas using its current properties.
+    drawBall(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.ballStartX, this.ballStartY, this.ballRadius, 0, Math.PI * 2);
+        ctx.fillStyle = this.ballColor;
+        ctx.fill();
+        ctx.closePath()
+    }
 }
+
+const ball = new Ball(200, 160, 20, 2, 2, 'red');
+ball.drawBall(ctx);
